4ada30b56532c5caab2f4ddfde700e7d78624580
Keshawn Sharper <Ksharper@studentmba.org>
2021-04-30 07:04:05 -0400

    grid list on the movie list

--- src/Movies/MovieList.js
+++ src/Movies/MovieList.js
@@ -2,7 +2,26 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import Movie from "./Movie";
 import { searchMovies } from "../reducers/actions";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+import Paper from "@material-ui/core/Paper";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    height: 140,
+    width: 100
+  },
+  control: {
+    padding: theme.spacing(2)
+  }
+}));
 function MovieList({ searchMovies, results, fav_movies, isLoading }) {
   const [movies, setMovies] = useState(true);
   const [value, setValue] = useState("");
@@ -10,12 +29,20 @@ function MovieList({ searchMovies, results, fav_movies, isLoading }) {
     setValue(e.target.value);
     searchMovies(value);
   };
+  const [spacing, setSpacing] = React.useState(2);
+  const classes = useStyles();
   return (
     <div className="App">
       <input value={value} onChange={(e) => handleChange(e)} />
-      {results.map((result) => (
-        <Movie Movie={result} />
-      ))}
+      <Grid container className={classes.root} spacing={2}>
+        {results.map((result) => (
+          <>
+            <Grid item xs={4}>
+              <Movie Movie={result} />
+            </Grid>
+          </>
+        ))}
+      </Grid>
     </div>
   );
 }
